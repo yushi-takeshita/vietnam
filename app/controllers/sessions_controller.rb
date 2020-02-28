82aa0f63b787db8d5acc2574e171a38a681a263c
otake <coffeein.lw@gmail.com>
2020-02-28 12:02:19 +0900

    Add failed login case
    Fix rubocop and test
    Add webdrivers gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
       flash.now[:danger] = "入力情報が正しくありません"

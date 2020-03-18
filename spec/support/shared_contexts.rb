3c6ae610c89b068db8f7aba146c8abe844b40268
otake <coffeein.lw@gmail.com>
2020-03-18 13:24:59 +0900

    Add shared contexts and examples

---
+++ spec/support/shared_contexts.rb
@@ -0,0 +1,11 @@
+# encoding: UTF-8
+
+shared_context "正しい情報が入力された場合" do
+  before "正しい情報を入力する" do
+    fill_in "user[name]", with: "テストユーザー"
+    fill_in "user[email]", with: "test1@example.com"
+    fill_in "user[password]", with: "password"
+    fill_in "user[password_confirmation]", with: "password"
+    find(".btn-primary").click
+  end
+end
---
+++ spec/support/shared_examples.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+
+shared_examples "ユーザー登録が成功すること" do
+  it {
+    # マイページへリダイレクトされる
+    @user = User.find_by(email: "test1@example.com")
+    expect(current_path).to eq user_path(@user)
+
+    # 登録成功のフラッシュが表示される
+    expect(page).to have_css ".alert-success"
+  }
+end
+shared_examples "ログイン状態であること" do
+  it {
+    # ヘッダーにマイページが表示される
+    find(".navbar-toggler").click
+    expect(page).to have_css ".mypage"
+
+    # ヘッダーからログアウト時に見えるリンクが除外される
+    expect(page).not_to have_css ".login"
+    expect(page).not_to have_css ".create-account"
+
+    # トップページに表示されていたサイト紹介文とログインフォームが除外される
+    visit root_path
+    expect(page).not_to have_css ".introduction"
+  }
+end
+shared_examples "ユーザー登録が失敗すること" do
+  it {
+    # 登録フォームに何も入力せずに登録ボタンをクリックする
+    # DBのユーザー数は増えない
+    expect { find(".btn-primary").click }.not_to change { User.count }
+
+    # 会員登録ページに戻る(「会員登録の入力」と表示されたまま)
+    expect(page).to have_css "h1.signup"
+
+    # エラーが表示される
+    expect(page).to have_css "#error_explanation"
+  }
+end

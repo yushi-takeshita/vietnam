52f9540b05ea08854902a63e994959bf2f1dd98d
otake <coffeein.lw@gmail.com>
2020-03-24 17:18:19 +0900

    Fix login bug from top page #12

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc)
     @user = User.new
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show

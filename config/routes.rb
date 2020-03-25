c6ba8f648a8a974a784f0a1e30eb7df8db8ad991
otake <coffeein.lw@gmail.com>
2020-03-25 14:28:01 +0900

    [WIP]Add password reset rooting #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
   root to: "posts#index"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
-
   resources :posts
   resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end

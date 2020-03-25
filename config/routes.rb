77fe7568f2e2b0aa6c09c72021c0d939497baa12
otake <coffeein.lw@gmail.com>
2020-03-25 14:18:34 +0900

    [WIP]Create password resets controller #16

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root to: "posts#index"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"

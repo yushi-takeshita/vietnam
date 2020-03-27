dddb8c18fa8974b0b95ff8688d0986ce4d91c53d
otake <coffeein.lw@gmail.com>
2020-03-27 17:28:11 +0900

    [WIP]Update mailer to actually send mail to user with gmail #16

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      redirect_to root_url, flash: { info: "メールに記載されたリンクからパスワード再設定ページへアクセスしてください" }
+      redirect_to root_url, flash: { info: "受信メールからパスワード再設定ページへアクセスして下さい ※迷惑メールBOXも確認して下さい" }
     else
       flash.now[:danger] = "メールアドレスが見つかりません"
       render "password_resets/new"

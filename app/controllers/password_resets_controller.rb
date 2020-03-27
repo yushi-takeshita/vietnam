0c7ddd17b8c8d16f0159f413174e33198128e0f1
otake <coffeein.lw@gmail.com>
2020-03-27 15:52:14 +0900

    [WIP]Add password reset mailer #16

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,16 @@
 class PasswordResetsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      redirect_to root_url, flash: { info: "メールに記載されたリンクからパスワード再設定ページへアクセスしてください" }
+    else
+      flash.now[:danger] = "メールアドレスが見つかりません"
+      render "password_resets/new"
+    end
   end
 
   def edit
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "Site Manager <noreply_to_you@example.com>"
   layout "mailer"
 end

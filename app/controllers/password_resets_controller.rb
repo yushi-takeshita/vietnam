dcd41d54f53695253b68b1eca797c552eaed8072
otake <coffeein.lw@gmail.com>
2020-03-27 18:47:10 +0900

    [WIP]Update edit view and authenticated? helper #16

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetsController < ApplicationController
     if @user
       @user.create_reset_digest
       @user.send_password_reset_email
-      redirect_to root_url, flash: { info: "受信メールからパスワード再設定ページへアクセスして下さい ※迷惑メールBOXも確認して下さい" }
+      redirect_to root_url, flash: { info: "受信メールからパスワード再設定ページへアクセスして下さい ※届いていない場合は迷惑メールBOXも確認して下さい" }
     else
       flash.now[:danger] = "メールアドレスが見つかりません"
       render "password_resets/new"
@@ -14,5 +14,9 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(email: params[:email])
+    unless @user && @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
   end
 end

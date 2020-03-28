595761416e96f37553a258a4ae9d1c792833ae46
otake <coffeein.lw@gmail.com>
2020-03-28 11:23:32 +0900

    [WIP]Complete adding reset password function without its test #16

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new; end
 
   def create
@@ -13,10 +17,43 @@ class PasswordResetsController < ApplicationController
     end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render "edit"
+    elsif @user.update_attributes(user_params)
+      login @user
+      redirect_to @user, flash: { success: "パスワード再設定が完了しました" }
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # before
+
+  def get_user
     @user = User.find_by(email: params[:email])
+  end
+
+  # 有効なユーザーか確認する
+  def valid_user
     unless @user && @user.authenticated?(:reset, params[:id])
       redirect_to root_url
+    end
+  end
+
+  # トークンが有効切れかどうか確認する
+  def check_expiration
+    if @user.password_reset_expired?
+      redirect_to new_password_reset_url, flash: { danger: "パスワード再設定の有効期限が切れています。初めからやり直して下さい。" }
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,10 @@ class User < ApplicationRecord
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   class << self
     # 渡された文字列のハッシュ値を返す
     def digest(string)

6d2be2fefc7854df5e23f1f3f68cc9fe1ba9f157
otake <coffeein.lw@gmail.com>
2020-04-05 18:37:02 +0900

    [WIP]Add vietnamese locale to user model #19

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Vietnam
     config.load_defaults 5.2
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
     config.i18n.enforce_available_locales = true
     config.i18n.available_locales = %i(ja vi)

99d42666502fbd93bfccbeacc8663a4cb46a7b69
otake <coffeein.lw@gmail.com>
2020-04-05 15:03:32 +0900

    [WIP]Add config.i18n.default_locale is ja in config/application.rb #19

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Vietnam
     config.load_defaults 5.2
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

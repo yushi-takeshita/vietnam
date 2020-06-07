e0c48573427894427f2c12738983a171cc586a34
otake <coffeein.lw@gmail.com>
2020-06-07 23:01:42 +0900

    [WIP]Improve system spec #23

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -8,13 +8,14 @@ RSpec.describe "暮らしに役立つサイト", type: :system do
 
   it "目次が見えていること" do
     expect(page).to have_selector "h5", text: I18n.t("home.useful_pages.目次")
-    expect(page).to have_selector "#section1", text: I18n.t("home.useful_pages.外国人技能実習機構")
-    expect(page).to have_selector "#section2", text: I18n.t("home.useful_pages.コミトモ")
+    expect(page).to have_selector "#section0", text: I18n.t("home.useful_pages.外国人技能実習機構")
+    expect(page).to have_selector "#section1", text: I18n.t("home.useful_pages.コミトモ")
   end
 
   it "項目をクリックすると項目へページ内ジャンプすること" do
     number = rand(0..1)
     all("li")[number].click_link
-    expect(current_url).to include "#section#{number + 1}"
+    # アンカーがURLに含まれていること
+    expect(current_url).to include "#section#{number}"
   end
 end

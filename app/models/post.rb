3f45e51fb4bd3b77a9a291db8e72f88a9e56f607
otake <coffeein.lw@gmail.com>
2020-06-10 09:45:25 +0900

    [WIP]Add title column to posts with validation test #28

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 600 }
   validates :user_id, presence: true
+  validates :title, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require "byebug"
 
 RSpec.describe "ポストモデル", type: :model do
   let(:user_a) { FactoryBot.create(:user, admin: true) }
-  let(:post_a) { user_a.posts.build(content: "Hello") }
+  let(:post_a) { user_a.posts.build(content: "Hello", title: "greetings") }
 
   describe "バリデーション" do
     it "正しい投稿であること" do
@@ -12,6 +12,12 @@ RSpec.describe "ポストモデル", type: :model do
     describe "user_id" do
       it "空白だとエラーになること" do
         post_a.user_id = nil
+        expect(post_a).not_to be_valid
+      end
+    end
+    describe "title" do
+      it "空白だとエラーになること" do
+        post_a.title = nil
         expect(post_a).not_to be_valid
       end
     end

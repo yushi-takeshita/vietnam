04ea294cab32f7947b018f9f91283afa576c3341
otake <coffeein.lw@gmail.com>
2020-06-16 09:20:58 +0900

    [WIP]Improve post model to avoid the unexpected parameter #28

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 600 }
   validates :user_id, presence: true
   validates :title, presence: true
+  def self.ransackable_attributes(auth_object = nil)
+    %w[content]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 end

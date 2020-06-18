036f9a8d30562153ef6142b921451ab3a5895a6b
otake <coffeein.lw@gmail.com>
2020-06-18 18:46:11 +0900

    [WIP]Improve layout of post index in user show page #28

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.all
+    @posts = @user.posts.all.page(params[:page]).per(10)
   end
 
   def new

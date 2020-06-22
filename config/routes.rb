84a7d1c25a420797a6dbdd1232b28fc789113602
otake <coffeein.lw@gmail.com>
2020-06-22 09:15:12 +0900

    Add category_path to routes #28

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :posts
     resources :users, except: [:index]
     resources :password_resets, only: [:new, :create, :edit, :update]
+    get "/category/:id", to: "categories#new", as: "category"
   end
 end

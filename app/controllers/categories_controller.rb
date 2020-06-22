da892f7ce3e422455806a078b38a94da42399d5c
otake <coffeein.lw@gmail.com>
2020-06-22 09:15:30 +0900

    Add categories controller #28

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,9 @@
+class CategoriesController < ApplicationController
+  def new
+    @children = Category.find(params[:parent_id]).children
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+end

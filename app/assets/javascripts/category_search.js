2d58cd4ffdaebe1684458f40274dc9b74e34b581
otake <coffeein.lw@gmail.com>
2020-06-24 10:30:56 +0900

    Add mouseover animation to child category with jQuery #28

--- app/assets/javascripts/category_search.js
+++ app/assets/javascripts/category_search.js
@@ -6,19 +6,25 @@ $(function () {
 
   $(".parent_category").mouseover(function () {
     var id = this.id;
-    $(".now-selected").removeClass("now-selected")
-    $('#' + id).addClass("now-selected")
+    $(".now-selected-gray").removeClass("now-selected-gray")
+    $('#' + id).addClass("now-selected-gray")
     $(".child_category").remove();
     $.ajax({
       type: 'GET',
       url: '/category/new',
       dataType: 'json',
-      data: { parent_id: id },
+      data: { parent_id: id }
     }).done(function (children) {
       children.forEach(function (child) {
         var html = buildChildHTML(child);
         $(".children_list").append(html);
       });
     });
+  });
+
+  $(document).on("mouseover", ".child_category", function () {
+    var id = this.id;
+    $(".now-selected-green").removeClass("now-selected-green");
+    $('#' + id).addClass("now-selected-green");
   });
 });

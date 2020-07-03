1962c22507a2cd76a9c66809611d4609169e964b
otake <coffeein.lw@gmail.com>
2020-07-03 10:17:11 +0900

    Improve layout of word search form #28

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.map(&:to_s).join("|")}/ do
-    root to: "home#top"
-    get "useful_pages", to: "home#useful_pages"
-    get "login", to: "sessions#new"
-    post "login", to: "sessions#create"
-    delete "logout", to: "sessions#destroy"
-    resources :users, except: [:index]
-    resources :password_resets, only: [:new, :create, :edit, :update]
+    controller :home do
+      root to: "home#top"
+      get "useful_pages", to: "home#useful_pages"
+    end
+
+    controller :sessions do
+      get "login", to: "sessions#new"
+      post "login", to: "sessions#create"
+      delete "logout", to: "sessions#destroy"
+    end
+
+    controller :users do
+      resources :users, except: [:index]
+    end
+
+    controller :password_resets do
+      resources :password_resets, only: [:new, :create, :edit, :update]
+    end
 
     controller :posts do
       resources :posts, except: [:index]
       get "categories/(:category_id)/index", to: "posts#index", as: "category"
     end
+
     controller :categories do
       get "categories/new", to: "categories#new", defaults: { format: "json" }
     end

0871101740fbac600d267b0e9f8deacbbe26d208
otake <coffeein.lw@gmail.com>
2020-07-04 15:39:42 +0900

    Make presence true of category_id in post model #28

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 600 }
   validates :user_id, presence: true
+  validates :category_id, presence: true
   validates :title, presence: true
 
   def self.ransackable_attributes(auth_object = nil)

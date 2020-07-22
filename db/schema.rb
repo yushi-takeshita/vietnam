f42a8d97121a46015bc44dda6eaa02739daa8dab
otake <coffeein.lw@gmail.com>
2020-07-22 09:33:52 +0900

    Delete chat_messages table and model #28

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_043021) do
+ActiveRecord::Schema.define(version: 2020_07_22_003055) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

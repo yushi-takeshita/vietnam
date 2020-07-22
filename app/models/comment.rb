84adf691872b809fa382402e7109d0563394b23a
otake <coffeein.lw@gmail.com>
2020-07-22 09:57:52 +0900

    Add validations to comment model #28

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 300 }
+  default_scope -> { order(created_at: :desc) }
 end

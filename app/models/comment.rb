74fc3f77dfe197f3d8d13cc8c03c9fe979424d2e
otake <coffeein.lw@gmail.com>
2020-07-22 10:15:28 +0900

    Add post_id to comment model #28

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  validates :user_id, presence: true
+  belongs_to :user, :post
+  validates :user_id, :post_id, presence: true
   validates :body, presence: true, length: { maximum: 300 }
   default_scope -> { order(created_at: :desc) }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_004739) do
+ActiveRecord::Schema.define(version: 2020_07_22_010701) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_07_22_004739) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id", "created_at"], name: "index_comments_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -79,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_004739) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"

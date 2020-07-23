66e0c3d73433a18dbb36c769f0b12d375172b0fa
otake <coffeein.lw@gmail.com>
2020-07-23 16:36:04 +0900

    Add create action to comments controller #28

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :logged_in_user
+
   def create
+    @comment = current_user.comments.build(body: params[:comment][:body], post_id: params[:post_id])
+    if @comment.save
+      redirect_to post_path(@comment.post), flash: { success: t(".flash.コメントしました") }
+    else
+      @post = Post.find(params[:post_id])
+      render "posts/show"
+    end
   end
 
   def destroy

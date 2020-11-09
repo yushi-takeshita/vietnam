87f76db0e9fded12adad801aebbd04395a3a2199
otake <coffeein.lw@gmail.com>
2020-11-09 20:41:49 +0900

    ReModify action mailer config to production #42

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "postone.herokuapp.com" }
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
     domain: "heroku.com",
